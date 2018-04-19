858a2887fba5fc29c78212211626062fbb040095
Nirjhar-EhsanulH <nirjhar.ehn@gmail.com>
2018-04-18 21:46:52 -0500

    abc

--- scripts/map.js
+++ scripts/map.js
@@ -29,6 +29,7 @@ var query= d3.select("#submit");
 
 query.on("click",function(){
 //map.on('load', load_data);
+
 load_data();
 
 });
@@ -50,6 +51,7 @@ query3.on("click",function(){
 //map.on('load', load_data);
 //draw_lines();
 //location.href="./index.html";
+console.log("abar")
 svg.remove();
 load_data();
 
@@ -63,7 +65,8 @@ load_data();
 
 function project(d) {
 
-            console.log("in");
+          //  console.log(d);
+            //console.log(map.project(new mapboxgl.LngLat(d[0], d[1])))
             return map.project(new mapboxgl.LngLat(d[0], d[1]));
 }
 
@@ -86,39 +89,49 @@ var count=0;
 
 
 
-function drawData(data1,data2) {
+function drawData(data) {
 console.log("draw data");
 //data=project_point(data);
             // Add circles
 //pick up points
+console.log(data)
 
-
-
+// console.log(data2)
 
 layout = svg.selectAll(".circle")
-        .data(data1);
+        .data(data);
 
 circles= layout .enter()
         .append("circle")
         .attr("class", "circle")
-        .attr("r", 4.5)
-        .attr("fill", "#2d2d86");
+        .attr("r", function(d){
+        //  console.log(d.size)
+          return d.size
+        })
+        .attr("fill", "#ff3333");
        // .on("click",function(d){start.x=d.x ; start.y=d.y; var temp= [project(d).x,project(d).y]; interpolate.push(temp); });
 
+// var ff= d3.selectAll("circle")
 
+// ff.data(sizeinfo)
+
+// ff.attr("r", function(d){
+//     console.log(d)
+//     return d
+//   })
 
 //dropoff points
 
-layout2 = svg.selectAll(".point")
-        .data(data2);
+// layout2 = svg.selectAll(".point")
+//         .data(data2);
 
-circles2= layout2 .enter()
-        .append("circle")
-        .attr("class", "point")
-        .attr("r", 4.5)
-        .attr("fill", " #ff3333");
-        //.on("click",function(d){end.x=d.x ; end.y=d.y;var temp= [project(d).x,project(d).y]; interpolate.push(temp);});
-            // Call the update function
+// circles2= layout2 .enter()
+//         .append("circle")
+//         .attr("class", "point")
+//         .attr("r", 4.5)
+//         .attr("fill", " #ff3333");
+//         //.on("click",function(d){end.x=d.x ; end.y=d.y;var temp= [project(d).x,project(d).y]; interpolate.push(temp);});
+//             // Call the update function
         update();
         map.on("viewreset", update);
         map.on("move",      update);
@@ -174,7 +187,7 @@ else
 
 
 var newCenter=[temp[0],temp[1]];
-console.log(dist);
+//console.log(dist);
 map.setZoom(Zoom);
 
 map.setCenter(newCenter);
@@ -193,14 +206,14 @@ if(count==0)
         
        // console.log(project(d).x);
         
-        circles.attr("cx", function(d,i) { return project(d).x;})
-                  .attr("cy", function(d,i) { return project(d).y;})
-                  .on("click",function(d){start[0]= d[0] ; start[1]=d[1]; var temp= [project(d).x,project(d).y]; interpolate.push(temp); });
+        circles.attr("cx", function(d,i) { return project(d.center).x;})
+                  .attr("cy", function(d,i) { return project(d.center).y;})
+             //     .on("click",function(d){start[0]= d[0] ; start[1]=d[1]; var temp= [project(d).x,project(d).y]; interpolate.push(temp); });
 
 
-        circles2.attr("cx", function(d,i) { return project(d).x;})
-                  .attr("cy", function(d,i) { return project(d).y;})
-                  .on("click",function(d){end[0]=d[0] ; end[1]=d[1];var temp= [project(d).x,project(d).y]; interpolate.push(temp);});
+        // circles2.attr("cx", function(d,i) { return project(d).x;})
+        //           .attr("cy", function(d,i) { return project(d).y;})
+        //           .on("click",function(d){end[0]=d[0] ; end[1]=d[1];var temp= [project(d).x,project(d).y]; interpolate.push(temp);});
         }
 
     function draw_lines(){
@@ -242,25 +255,102 @@ var startTime = (+document.getElementById("dropdown1").value)
 var endTime = (+document.getElementById("dropdown2").value)
     console.log(date_sel,startTime,endTime)
 
+var temporalID = './data/t'
+var fdpID = './data/FDP'
 
-var mainID = 'yellow_tripdata_2016-01-'
-var fileID = mainID.concat(date_sel.toString().concat(".json")) // Converts dropdown selection to corresponding filename
-var taxiData = {}
+var tFile = temporalID.concat(date_sel.toString().concat(".json"))
+var fdpFile = fdpID.concat(date_sel.toString().concat(".json"))
 
 
-d3.json(fileID, function(d){
-  // taxiData = d
-  console.log(d.length)
-      var pick= [];
-      var drop= [];
-    for (var j = 1; j <1000; j++) {
-          if(d[j].pickupHr>=startTime && d[j].pickupHr<=endTime){
-          pick.push(d[j].pickupLoc)
-          drop.push(d[j].dropoffLoc)
-        }
+d3.json(tFile, function(d1){
+  d3.json(fdpFile, function(d2){
+    temporalFilter(d1,d2,startTime,endTime)
+    
+  })
+
+})
+// var mainID = 'yellow_tripdata_2016-01-'
+// var fileID = mainID.concat(date_sel.toString().concat(".json")) // Converts dropdown selection to corresponding filename
+// var taxiData = {}
+// console.log(fileID)
+
+
+// d3.json(fileID, function(d){
+  
+//   console.log(d.length)
+//       var pick= [];
+//       var drop= [];
+//     for (var j = 1; j <1000; j++) {
+//           if(d[j].pickupHr>=startTime && d[j].pickupHr<=endTime){
+//           pick.push(d[j].pickupLoc)
+//           drop.push(d[j].dropoffLoc)
+//         }
+//     }
+//     console.log(pick)
+//    drawData(pick,drop);})
+}
+
+function temporalFilter(d1,d2,t_start,t_end){
+  var cluster_center = []
+  var cluster_info = []
+ var cluster_size = []
+ var fdp_info = []
+  // var cluster_data.dropoff = []
+//   console.log(d1)
+// console.log(d2)
+  for (var i=0; i<d1.length;i++){
+    
+    var pickup=0
+    var dropoff=0
+    var dest_num=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    for (var j=t_start; j<t_end;j++){
+      pickup += d1[i].pickup_point[j]
+      dropoff += d1[i].dropoff_point[j]
+      for (var k=0; k<30;k++){
+        dest_num[k] += d1[i].destination[j][k]
+       
+        
+      }
+
     }
-    console.log(pick)
-   drawData(pick,drop);})
+    cluster_info.push({"center":d1[i].center, "size": (pickup+dropoff), "pickup":pickup, "dropoff":dropoff, "destination":dest_num})
+    cluster_size.push(pickup+dropoff)
+    
+  }
+
+  var rmax=d3.max(cluster_size)
+ // console.log(rmax)
+  for (var i=0;i<cluster_info.length;i++){
+    cluster_info[i].size = (cluster_info[i].size*20)/rmax
+  }
+
+
+  var fare_num=[0,0,0,0,0,0]
+  var distance_num=[0,0,0,0,0,0]
+  var passenger_num=[0,0,0,0,0,0]
+  for (var i=t_start; i<t_end;i++){
+    for (var j=0; j<6;j++){
+      fare_num[j] += d2[0].fare_hist[i][j]
+      distance_num[j] += d2[0].distance_hist[i][j]
+      passenger_num[j] += d2[0].passenger_hist[i][j]
+    }
+
+  }
+  fdp_info.push({"fare_hist":fare_num, "distance_hist":distance_num, "passenger_hist":passenger_num})
+
+  console.log(cluster_info)
+  console.log(fdp_info)
+  
+
+  drawData(cluster_info)
+  fareChart(fdp_info[0].fare_hist)
+  distanceChart(fdp_info[0].distance_hist)
+  passengerChart(fdp_info[0].passenger_hist)
+  
 }
 
 }
+
+
+
---
+++ scripts/util.js
@@ -0,0 +1,19 @@
+function fareChart(data){
+  console.log(data)
+  // var bb=["0-4","4-8","8-12","12-16","16-20",">20"]
+  // var fd=[]
+  // for (i=0;i<6;i++){
+  // 	fd.push({"bin": bb[i], "fare": data[i]})
+  // }
+  // console.log(fd)
+
+
+}
+
+function distanceChart(data){
+	console.log(data)
+}
+
+function passengerChart(data){
+	console.log(data)
+}
